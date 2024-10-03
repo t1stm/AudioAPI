02d6eda7fed8f74bc0ac7008103c0740f58f62a6
Kristian Gergov <kristian@gergov.bg>
2024-10-04 00:46:06 +0300

    Include ThumbnailUrl

--- AudioManager/Platforms/Optional/HasThumbnail.cs
+++ AudioManager/Platforms/Optional/HasThumbnail.cs
@@ -1,6 +1,9 @@
+using System.Text.Json.Serialization;
+
 namespace AudioManager.Platforms.Optional;
 
 public interface HasThumbnail
 {
+    [JsonIgnore]
     public string? ThumbnailUrl { get; }
 }
