11d02b1000c656fca218a205a04f0eb435d34675
Kristian Gergov <kristian@gergov.bg>
2024-10-04 14:42:59 +0300

    Fix YouTubeResult.cs

--- AudioManager/Platforms/YouTube/YouTubeResult.cs
+++ AudioManager/Platforms/YouTube/YouTubeResult.cs
@@ -7,7 +7,7 @@ public sealed class YouTubeResult : PlatformResult, HasThumbnail
     public string? ThumbnailUrl { get; init; }
     public override string GetDownloadUrl()
     {
-        var pure_id = ID.Split("://")[1..];
-        return $"https://www.youtube.com/watch?v={ID}";
+        var pure_id = ID.Split("://")[1];
+        return $"https://www.youtube.com/watch?v={pure_id}";
     }
 }
