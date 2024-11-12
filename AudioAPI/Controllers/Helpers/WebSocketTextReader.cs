119a9f34bf07d80d5c4cbdbfcf549aaf7bb2fd9b
Kristian Gergov <kristian@gergov.bg>
2024-11-12 02:50:19 +0200

    Add a try-catch in the WebSocketTextReader.cs

--- AudioAPI/Controllers/Helpers/WebSocketTextReader.cs
+++ AudioAPI/Controllers/Helpers/WebSocketTextReader.cs
@@ -2,7 +2,6 @@ using System.Buffers;
 using System.Net.WebSockets;
 using System.Text;
 using Result;
-using TagLib.IFD.Tags;
 
 namespace AudioAPI.Controllers.Helpers;
 
@@ -13,32 +12,40 @@ public class WebSocketTextReader
     public async Task<Result<string, WebSocketReadStatus>> ReadWholeMessageAsync(WebSocket web_socket,
         CancellationToken? cancellation_token = default)
     {
-        cancellation_token ??= new CancellationToken();
-        _builder.Clear();
-        if (web_socket.State != WebSocketState.Open) 
-            return Result<string, WebSocketReadStatus>.Error(WebSocketReadStatus.Closed);
-        
-        using var buffer = MemoryPool<byte>.Shared.Rent(1024 * 32);
-        ValueWebSocketReceiveResult receive_result;
-
-        do
+        try
         {
-            receive_result = await web_socket.ReceiveAsync(buffer.Memory, cancellation_token.Value);
-            if (receive_result.MessageType != WebSocketMessageType.Text) continue;
-            
-            var data_slice = buffer.Memory[..receive_result.Count];
-            _builder.Append(Encoding.UTF8.GetString(data_slice.Span));
+            cancellation_token ??= new CancellationToken();
+            _builder.Clear();
+            if (web_socket.State != WebSocketState.Open)
+                return Result<string, WebSocketReadStatus>.Error(WebSocketReadStatus.Closed);
 
-            if (receive_result.MessageType != WebSocketMessageType.Close) continue;
-            return Result<string, WebSocketReadStatus>.Error(WebSocketReadStatus.Closed);
+            using var buffer = MemoryPool<byte>.Shared.Rent(1024 * 32);
+            ValueWebSocketReceiveResult receive_result;
+
+            do
+            {
+                receive_result = await web_socket.ReceiveAsync(buffer.Memory, cancellation_token.Value);
+                if (receive_result.MessageType != WebSocketMessageType.Text) continue;
+
+                var data_slice = buffer.Memory[..receive_result.Count];
+                _builder.Append(Encoding.UTF8.GetString(data_slice.Span));
+
+                if (receive_result.MessageType != WebSocketMessageType.Close) continue;
+                return Result<string, WebSocketReadStatus>.Error(WebSocketReadStatus.Closed);
+            } while (!receive_result.EndOfMessage);
+
+            return Result<string, WebSocketReadStatus>.Success(_builder.ToString());
         }
-        while (!receive_result.EndOfMessage);
-        return Result<string, WebSocketReadStatus>.Success(_builder.ToString());
+        catch (Exception e)
+        {
+            return Result<string, WebSocketReadStatus>.Error(WebSocketReadStatus.Unknown);
+        }
     }
 }
 
 public enum WebSocketReadStatus
 {
     None,
-    Closed
+    Closed,
+    Unknown
 }
