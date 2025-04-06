e329ced8ff84bd28d6cb90492e86554a9f972a86
Kristian Gergov <kristian@gergov.bg>
2025-04-06 20:07:10 +0300

    Use stack allocation for Levenshtein distance calculations where applicable

--- AudioManager/Utils/LevenshteinDistance.cs
+++ AudioManager/Utils/LevenshteinDistance.cs
@@ -12,31 +12,45 @@ public static class LevenshteinDistance
     public static int ComputeStrict(string? s, string? t)
     {
         if (string.IsNullOrEmpty(s)) return string.IsNullOrEmpty(t) ? 0 : t.Length;
-
         if (string.IsNullOrEmpty(t)) return s.Length;
 
+        var source_span = s.AsSpan();
+        var target_span = t.AsSpan();
+        
+        return ComputeStrict(source_span, target_span);
+    }
+
+    public static int ComputeStrict(ReadOnlySpan<char> s, ReadOnlySpan<char> t)
+    {
         var n = s.Length;
         var m = t.Length;
-        var d = new int[n + 1, m + 1];
+        
+        if (n == 0) return m;
+        if (m == 0) return n;
 
-        for (var i = 0; i <= n; d[i, 0] = i++)
-        {
-        }
+        var m1 = m + 1;
+        const int MAX_STACK_SIZE = 1 << 10;
+        
+        var prev = m1 > MAX_STACK_SIZE ? new int[m1] : stackalloc int[m1];
+        var curr = m1 > MAX_STACK_SIZE ? new int[m1] : stackalloc int[m1];
 
-        for (var j = 1; j <= m; d[0, j] = j++)
-        {
-        }
+        for (var j = 0; j <= m; j++)
+            prev[j] = j;
 
         for (var i = 1; i <= n; i++)
+        {
+            curr[0] = i;
             for (var j = 1; j <= m; j++)
             {
-                var cost = t[j - 1] == s[i - 1] ? 0 : 1;
-                var min1 = d[i - 1, j] + 1;
-                var min2 = d[i, j - 1] + 1;
-                var min3 = d[i - 1, j - 1] + cost;
-                d[i, j] = Math.Min(Math.Min(min1, min2), min3);
+                var cost = s[i - 1] == t[j - 1] ? 0 : 1;
+                curr[j] = Math.Min(Math.Min(curr[j - 1] + 1, prev[j] + 1), prev[j - 1] + cost);
             }
 
-        return d[n, m];
+            var temp = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev[m];
     }
 }
